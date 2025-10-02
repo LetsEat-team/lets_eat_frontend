f962d33517e6abc8a0f3d87b0521bb24382b93e1
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-10-03 00:43:33 +0900

    fix: #6 CharaTalk에 캐릭터 이름

--- src/components/CharaTalk.tsx
+++ src/components/CharaTalk.tsx
@@ -1,12 +1,14 @@
 type CharaTalkProps = {
   children: React.ReactNode;
+  charName: string;
 };
 
-const CharaTalk = ({ children }: CharaTalkProps) => {
-  const charName = '캐릭터이름';
+const CharaTalk = ({ children, charName }: CharaTalkProps) => {
   return (
     <div className="w-[335px] h-[118px] bg-white relative rounded-[5px] border border-maingreen flex justify-center items-center">
-      <div className='w-[77px] h-[26px] absolute top-[-14px] left-[10px] rounded-[50px] bg-maingreen font-medium text-[12px] flex justify-center items-center text-white'>{charName}</div>
+      <div className='w-[77px] h-[26px] absolute top-[-14px] left-[10px] rounded-[50px] bg-maingreen font-medium text-[12px] flex justify-center items-center text-white'>
+        {charName}
+      </div>
       <div className="text-center px-[18px] font-medium whitespace-pre-line text-[16px]">{children}</div>
     </div>
   );
--- src/pages/child_card/ChildCardOnBoard.tsx
+++ src/pages/child_card/ChildCardOnBoard.tsx
@@ -9,7 +9,7 @@ export default function ChildCardOnBoard({ onFinish }: ChildCardOnBoardProps) {
   return (
     <div>
       <h2>환영합니다!</h2>
-      <CharaTalk>{`아동급식카드를 등록하면 `} 
+      <CharaTalk charName="캐릭터이름">{`아동급식카드를 등록하면 `} 
         <span className="text-maingreen">냠냠패스</span>
         {`가 생겨요!\n`}<span className="text-maingreen">냠냠패스</span>
         {`로 메뉴를 구매하고\n가게에 보여줄 `}
