27cb0e54426cd89365d7ff0d5b52a74797dfd16d
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-10-03 01:51:21 +0900

    fix: ChildCardONBoard

--- src/pages/child_card/ChildCardOnBoard.tsx
+++ src/pages/child_card/ChildCardOnBoard.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button";
 import CharaTalk from "../../components/CharaTalk";
 
 type ChildCardOnBoardProps = {
@@ -7,16 +8,16 @@ type ChildCardOnBoardProps = {
 export default function ChildCardOnBoard({ onFinish }: ChildCardOnBoardProps) {
 
   return (
-    <div>
-      <h2>환영합니다!</h2>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[258px] h-[258px] mt-[71px] mb-[67px] rounded-full bg-gray-200"></div>
       <CharaTalk charName="캐릭터이름">{`아동급식카드를 등록하면 `} 
         <span className="text-maingreen">냠냠패스</span>
         {`가 생겨요!\n`}<span className="text-maingreen">냠냠패스</span>
         {`로 메뉴를 구매하고\n가게에 보여줄 `}
-        <span className="text-maingreen">방문증</span>
+        <span className="text-maingreen">티켓</span>
         {`을 받아봐요~`}
       </CharaTalk>
-      <button onClick={onFinish}>온보딩 완료</button>
+      <Button onClick={onFinish} className="mt-[20px]">둘러보기</Button>
     </div>
   );
 }
