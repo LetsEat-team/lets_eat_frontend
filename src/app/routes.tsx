031d3185f9b90c25c352ea7bbc87534b36b7cff7
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-10-03 02:39:36 +0900

    feat:ChildCardOnBoard2 & useContextOnboard

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter,Navigate} from "react-router-dom";
 import App from "./App";
 import LoginPage from "../pages/LoginPage";
 import ChildCardWrapper from "../components/ChildCardWrapper";
+import ChildCardOnBoard2 from "../pages/child_card/ChildCardOnBoard2";
 
 export const router = createBrowserRouter([
   {
@@ -14,7 +15,9 @@ export const router = createBrowserRouter([
 
     //{ path: "/페이지", element: <페이지/> },
 
-    {path: "/childcard", element:<ChildCardWrapper/>} //아동급식카드
+    {path: "/childcard", element:<ChildCardWrapper/>}, //아동급식카드
+    { path: "/childcard/onboard2", element: <ChildCardOnBoard2 /> },
+
     ],
   },
 ]);
--- src/components/ChildCardWrapper.tsx
+++ src/components/ChildCardWrapper.tsx
@@ -1,11 +1,12 @@
-import { useOnboardingCheck } from "../hooks/useOnboardingCheck";
 import ChildCard from "../pages/child_card/ChildCard";
 import ChildCardOnBoard from "../pages/child_card/ChildCardOnBoard";
 
 export default function ChildCardWrapper() {
-  const { needsOnboarding, completeOnboarding } = useOnboardingCheck();
+  const needsOnboarding = true;
+
   if (needsOnboarding) {
-    return <ChildCardOnBoard onFinish={completeOnboarding} />;
+    return <ChildCardOnBoard  />;
   }
   return <ChildCard />;
 }
+
