2c1ef52f238e05a7232a07d4f366eaec6c9cf366
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-10-03 00:40:24 +0900

    feat:#6 CharaTalk

--- src/pages/child_card/ChildCardOnBoard.tsx
+++ src/pages/child_card/ChildCardOnBoard.tsx
@@ -1,3 +1,5 @@
+import CharaTalk from "../../components/CharaTalk";
+
 type ChildCardOnBoardProps = {
   onFinish: () => void;  //onBoarding 끝났는지 여부
 };
@@ -5,9 +7,15 @@ type ChildCardOnBoardProps = {
 export default function ChildCardOnBoard({ onFinish }: ChildCardOnBoardProps) {
 
   return (
-    <div className="p-5 rounded-lg bg-[#F8F8F8E0] backdrop-blur-[27px] shadow-md">
+    <div>
       <h2>환영합니다!</h2>
-      <p>처음 방문하셨군요. 온보딩 내용을 확인해 주세요.</p>
+      <CharaTalk>{`아동급식카드를 등록하면 `} 
+        <span className="text-maingreen">냠냠패스</span>
+        {`가 생겨요!\n`}<span className="text-maingreen">냠냠패스</span>
+        {`로 메뉴를 구매하고\n가게에 보여줄 `}
+        <span className="text-maingreen">방문증</span>
+        {`을 받아봐요~`}
+      </CharaTalk>
       <button onClick={onFinish}>온보딩 완료</button>
     </div>
   );
