ae902694d27e4ae2a5c5454249a890c8738e41c8
oculo0204 <oculo0204@gmail.com>
2025-10-11 19:53:21 +0900

    feat: #15 convyList complted

--- src/components/ConvyItem.tsx
+++ src/components/ConvyItem.tsx
@@ -5,7 +5,7 @@ interface ConvyItemProps {
 
 const ConvyItem = ({ image, name }: ConvyItemProps) => {
   return (
-    <div className="flex flex-col items-center justify-center w-[140px] h-[140px] border border-maingreen rounded-md shadow-sm bg-white">
+    <div className="flex flex-col items-center justify-center w-[140px] h-[140px] border rounded-md shadow-[2px_2px_2px_rgba(0,0,0,0.2)]">
       <img src={image} alt={name} className="w-[95px] h-[95px] mt-[8px] rounded-full bg-gray-300" />
       <p className="mt-1 text-textblack font-bold text-[16px]">{name}</p>
     </div>
